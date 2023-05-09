03fc9daa44082af3fac9988277efe2974648a031
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-05-09 11:54:51 -0300

    Iniciando Dia 2

--- delivery_backoffice/lib/src/app_module.dart
+++ delivery_backoffice/lib/src/app_module.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import './modules/home/home_page.dart';
+import 'modules/core/core_module.dart';
 import 'modules/login/login_module.dart';
 
 
 class AppModule extends Module {
-  @override
-  List<Bind> get binds => [];
 
+  @override
+  List<Module> get imports => [
+    CoreModule(),
+  ];
+  
   @override
   List<ModularRoute> get routes => [
     ModuleRoute('/login', module: LoginModule()),
--- delivery_backoffice/lib/src/modules/login/login_module.dart
+++ delivery_backoffice/lib/src/modules/login/login_module.dart
@@ -1,15 +1,23 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import '../../repositories/auth/auth_repository.dart';
+import '../../repositories/auth/auth_repository_impl.dart';
+import '../../services/auth/login_service.dart';
+import '../../services/auth/login_service_impl.dart';
 import 'login_page.dart';
 
 class LoginModule extends Module {
+  @override
+  List<Bind> get binds => [
+        Bind.lazySingleton<AuthRepository>(
+          (i) => AuthRepositoryImpl(i()),
+        ),
+        Bind.lazySingleton<LoginService>(
+          (i) => LoginServiceImpl(i(), i()),
+        )
+      ];
 
-   @override
-   List<Bind> get binds => [];
-
-   @override
-   List<ModularRoute> get routes => [
-      ChildRoute('/', child: (context, args) => const LoginPage())
-   ];
-
+  @override
+  List<ModularRoute> get routes =>
+      [ChildRoute('/', child: (context, args) => const LoginPage())];
 }
