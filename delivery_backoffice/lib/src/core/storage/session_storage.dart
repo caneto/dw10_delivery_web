ba38f3b6762f8479d100b32275dd3935f3277385
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2023-05-09 09:09:40 -0300

    Aula Dia 1 Finalizada

--- delivery_backoffice/lib/src/core/storage/session_storage.dart
+++ delivery_backoffice/lib/src/core/storage/session_storage.dart
@@ -1,7 +1,16 @@
-
+import 'dart:html';
 
 import 'storage.dart';
 
 class SessionStorage extends Storage{
+  
+  @override
+  String getData(String key) => window.sessionStorage[key] ?? '';
+
+  @override
+  void setData(String key, String value) => window.sessionStorage[key] = value;
+
+  @override
+  void clean() => window.sessionStorage.clear();
   
 }
